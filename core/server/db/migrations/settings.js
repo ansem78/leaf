516b6d791ebbf9e3c1a93203d67e0f3020157618
Alexandro Morato <alexandro1@email.it>
2017-03-11 13:56:08 +0100

    Modificato il model della tabella settings

--- core/server/db/migrations/settings.js
+++ core/server/db/migrations/settings.js
@@ -2,18 +2,8 @@
 exports.up = function(knex) {
   return knex.schema
     .createTableIfNotExists('settings', function(table) {
-        table.uuid('_id').notNullable().primary();
-        table.string('title').notNullable().defaultTo('');
-        table.string('slug').notNullable().defaultTo('').unique();
-        table.string('author').notNullable().defaultTo('').references('_id').inTable('users');
-        table.text('excerpt').notNullable().defaultTo('');
-        table.text('content','longtext').notNullable().defaultTo('');
-        table.integer('featured').notNullable().defaultTo(0);
-        table.string('status').notNullable().defaultTo('draft');
-        table.string('type').notNullable().defaultTo('post');
-        table.string('comments_status').notNullable().defaultTo('open');
-        table.integer('comments_count').notNullable().defaultTo(0);
-        table.timestamps();
+        table.string('name').notNullable().primary();
+        table.text('value','longtext').notNullable().defaultTo('');
     });
 };
 exports.down = function(knex) {
