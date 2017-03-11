c6dcb0c692eda4f95bcf1c59fe7cb32bfd18863f
arpho <damicogiuseppe77@gmail.com>
2017-03-11 19:39:38 +0100

    ripristinato knexfile, spostato migrations nel root dell progetto, messo a punto lo script per lanciare  la migrazione di knex

--- core/server/db/bookshelf.js
+++ core/server/db/bookshelf.js
@@ -1,5 +1,6 @@
-const config = require('../config');
+const config = require('../config'); // non troverà nessun config.js
 
+config.argv().env().file({file:'../config/config.json}) // imposto il path corretto per config.json
 const knex = require('knex')(config.get('database'));
 
 const bookshelf = require('bookshelf')(knex);
