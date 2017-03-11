978b807dcff5f33454f1c44b7bcda5bc0668aaad
Alexandro Morato <alexandro1@email.it>
2017-03-11 21:27:07 +0100

    Ripristinata la lettura dal file config.json

--- core/server/db/bookshelf.js
+++ core/server/db/bookshelf.js
@@ -1,7 +1,7 @@
-const config = require('../config'); // non troverà nessun config.js
+const config = require('../config'); // Carica il file /core/server/config/index.js che esporta l'oggetto nconf ottenuto leggendo il file config.json.
 
-config.argv().env().file({file:'../config/config.json}) // imposto il path corretto per config.json
-const knex = require('knex')(config.get('database'));
+//config.argv().env().file({file:'../config/config.json}) // imposto il path corretto per config.json
+const knex = require('knex')(config.get('database')); // Istanzia Knex.js passando l'oggetto con i parametri di connessione.
 
 const bookshelf = require('bookshelf')(knex);
 
