d251d8419887854623ecf5b9e5e9fa286524eb37
Alexandro Morato <alexandro1@email.it>
2017-03-14 09:49:04 +0100

    Passato Promise ai metodi

--- migrations/navigations.js
+++ migrations/navigations.js
@@ -1,15 +1,19 @@
 'use strict';
-exports.up = function(knex) {
-  return knex.schema
-    .createTableIfNotExists('navigation', function(table) {
-        table.uuid('_id').notNullable().primary();
-        table.string('name').notNullable().unique();
-        table.text('url','longtext').notNullable().unique();
-        table.integer('order').notNullable().defaultTo(0);
-        table.timestamps();
-    });
+
+exports.up = function(knex,Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('navigation',function(table) {
+          table.uuid('_id').notNullable().primary();
+          table.string('name').notNullable().unique();
+          table.text('url','longtext').notNullable().unique();
+          table.integer('order').notNullable().defaultTo(0);
+          table.timestamps();
+      })
+    ]);
 };
-exports.down = function(knex) {
-  return knex.schema
-    .dropTable('navigation');
+
+exports.down = function(knex,Promise) {
+  return Promise.all([
+    knex.schema.dropTable('navigation')
+  ]);
 };
