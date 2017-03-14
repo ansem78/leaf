fceb1d066e4fbe83f1734b836d12bfd7d1d32b6a
Alexandro Morato <alexandro1@email.it>
2017-03-14 09:54:55 +0100

    Passato Promise ai metodi

--- migrations/users.js
+++ migrations/users.js
@@ -1,7 +1,8 @@
 'use strict';
-exports.up = function(knex) {
-  return knex.schema
-    .createTableIfNotExists('users', function(table) {
+
+exports.up = function(knex,Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('users',function(table) {
         table.uuid('_id').notNullable().primary();
         table.string('email').notNullable().unique();
         table.string('password').notNullable().defaultTo('');
@@ -12,9 +13,12 @@ exports.up = function(knex) {
         table.text('description').notNullable().defaultTo('');
         table.string('role').notNullable().defaultTo('author');
         table.timestamps();
-    });
+    })
+  ]);
 };
-exports.down = function(knex) {
-  return knex.schema
-    .dropTable('users');
+
+exports.down = function(knex,Promise) {
+  return Promise.all([
+    knex.schema.dropTable('users')
+  ]);
 };
