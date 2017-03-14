2f68ea5d67a6d72dd96e3767a99630343bedc1c3
Alexandro Morato <alexandro1@email.it>
2017-03-14 09:57:02 +0100

    Modificata l'estensione del file in .db

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,9 @@
 'use strict';
+
 module.exports = {
-  client: 'sqlite3',
-  connection: {
-    filename: 'core/server/db/content/data/leaf.sqlite3',
+  client : 'sqlite3',
+  connection : {
+    filename : 'core/server/db/content/data/leaf.db'
   },
-  useNullAsDefault: true,
+  useNullAsDefault : true
 };
