e901f5aa131347f32acfa6e1bb4bfdee821f29af
Alexandro Morato <alexandro1@email.it>
2017-03-14 09:53:53 +0100

    Passato Promise ai metodi

--- migrations/tags.js
+++ migrations/tags.js
@@ -1,14 +1,18 @@
 'use strict';
-exports.up = function(knex) {
-  return knex.schema
-    .createTableIfNotExists('tags', function(table) {
+
+exports.up = function(knex,Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('tags',function(table) {
         table.uuid('_id').notNullable().primary();
         table.string('name').notNullable().unique();
         table.string('slug').notNullable().unique();
         table.timestamps();
-    });
+    })
+  ]);
 };
-exports.down = function(knex) {
-  return knex.schema
-    .dropTable('tags');
+
+exports.down = function(knex,Promise) {
+  return Promise.all([
+    knex.schema.dropTable('tags')
+  ]);
 };
