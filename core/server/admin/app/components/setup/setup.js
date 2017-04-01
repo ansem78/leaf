0d7c1ce55c015b9a5b0d4d9cf1ca53c63f6532d7
Alexandro Morato <alexandro1@email.it>
2017-04-01 13:29:35 +0200

    Aggiunto template + controller per il setup

---
+++ core/server/admin/app/components/setup/setup.js
@@ -0,0 +1,11 @@
+define(['angular'],function(angular) {
+
+  var app = angular.module('Leaf');
+
+  app.register.controller('setupController',['$rootScope','$scope',function($rootScope,$scope) {
+
+
+
+  }]);
+
+});
