dee4025389305dcb09390e3b33714aff40b9dba4
Alexandro Morato <alexandro1@email.it>
2017-04-01 13:23:44 +0200

    Aggiornati e aggiunti template e controller

---
+++ core/server/admin/app/components/login.js
@@ -0,0 +1,11 @@
+define(['angular'],function(angular) {
+
+  var app = angular.module('Leaf');
+
+  app.register.controller('loginController',['$rootScope','$scope',function($rootScope,$scope) {
+
+
+
+  }]);
+
+});
