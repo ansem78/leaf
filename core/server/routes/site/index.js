171c888845924a4b6d542e06eca6192130f2a956
Alexandro Morato <alexandro1@email.it>
2017-04-01 13:09:42 +0200

    Aggiunto argomento next a tutte le route

--- core/server/routes/site/index.js
+++ core/server/routes/site/index.js
@@ -5,37 +5,37 @@ const router = express.Router();
 // ---- Site. ----
 
 // Get homepage.
-router.get('/',function(req,res) {
+router.get('/',function(req,res,next) {
 
 });
 
 // Get a post.
-router.get('/posts/:post',function(req,res) {
+router.get('/posts/:post',function(req,res,next) {
 
 });
 
 // Get a page.
-router.get('/pages/:page',function(req,res) {
+router.get('/pages/:page',function(req,res,next) {
 
 });
 
 // Get search page.
-router.get('/search/:search',function(req,res) {
+router.get('/search/:search',function(req,res,next) {
 
 });
 
 // Get an archive page.
-router.get('/archives/:date',function(req,res) {
+router.get('/archives/:date',function(req,res,next) {
 
 });
 
 // Get an author page.
-router.get('/authors/:author',function(req,res) {
+router.get('/authors/:author',function(req,res,next) {
 
 });
 
 // Get a tag page.
-router.get('/tags/:tag',function(req,res) {
+router.get('/tags/:tag',function(req,res,next) {
 
 });
 
