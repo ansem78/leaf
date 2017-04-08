4e1921345b90280b38e18f8a696af6c5f1664844
arpho <damicogiuseppe77@gmail.com>
2017-04-08 16:47:50 +0200

    bug fixing

--- core/server/utilities/user/login.js
+++ core/server/utilities/user/login.js
@@ -1,11 +1,12 @@
 module.exports = function(email,password,cb){
+             console.log('ok//////////////////////////////////////////////////')
     const Users = require('../../models/users');
          new Users().query({where:{email:email}}).fetchAll().then((user) =>{
          const crypto = require("crypto-js")
          const salt = {}
          salt.words = user.toJSON()[0].salt.split(',')
          salt.sigBytes = 16
-         const hashed_password = hashed_password = require('./cryptoWrapper').hashPassword(password,salt)//crypto.PBKDF2(password, salt, { keySize: 512/32, iterations: 1000 }) 
+         const hashed_password = hashed_password = require('./cryptoWrapper').hashPassword(password,salt)//crypto.PBKDF2(password, salt, { keySize: 512/32, iterations: 1000 })
          const user_verified = user.toJSON()[0].password === hashed_password.words.join()
          const out = {logged:user_verified};
          if (user_verified){
@@ -13,7 +14,7 @@ module.exports = function(email,password,cb){
              const token = Token.generate(128)
              out.token = token;
              const cache = require('../').cache
-             cache.setCache(token,email)
+             cache.setCache(token,email)                                                                                       )
              out.token = token
          }
          cb(out);
