4b66f9b9698593a0a426929c42d89b0315f4e6c5
arpho <damicogiuseppe77@gmail.com>
2017-04-10 18:13:43 +0200

    rifattorizzato login, per gestire  login fallito, creato test per login fallito

--- core/server/utilities/user/login.js
+++ core/server/utilities/user/login.js
@@ -1,23 +1,34 @@
 module.exports = function(email,password,cb){
+                     const out = {}
          const Users = require('../../models/users');
          new Users().query({where:{email:email}}).fetchAll().then((user) =>{
-         const crypto = require("crypto-js")
-         const salt = {}
-         salt.words = user.toJSON()[0].salt.split(',')
-         salt.sigBytes = 16
-         const hashed_password = hashed_password = require('./cryptoWrapper').hashPassword(password,salt)//crypto.PBKDF2(password, salt, { keySize: 512/32, iterations: 1000 })
-         const user_verified = user.toJSON()[0].password === hashed_password.words.join()
-         const out = {logged:user_verified};
-         if (user_verified){
-             const Token = randtoken = require('neo-rand-token')
-             const token = Token.generate(128)
-             out.token = token;
-             const loggedUser = user.toJSON()[0]
-             const NodeCache = require( "node-cache" );
-             const cache = require('../').cache;
-             cache.setCache(token,user.toJSON()[0])
-             out.token = token
-         }
+             if(user.length==0){ //utente non registrat
+                 out.user_verified = false;
+                 out.registered_uer = false;
+                 out.msg = 'email  o password non validi'
+             }
+             else{
+                 const crypto = require("crypto-js");
+                 const salt = {};
+                 salt.words = user.toJSON()[0].salt.split(',');
+                 salt.sigBytes = 16;
+                 const hashed_password = hashed_password = require('./cryptoWrapper').hashPassword(password,salt)//crypto.PBKDF2(password, salt, { keySize: 512/32, iterations: 1000 });
+                 out.user_verified = user.toJSON()[0].password === hashed_password.words.join();
+                 if (out.user_verified){
+                     out.user_logged = true;
+                     const Token = randtoken = require('neo-rand-token');
+                     const token = Token.generate(128);
+                     out.token = token;
+                     const loggedUser = user.toJSON()[0];
+                     const NodeCache = require( "node-cache" );
+                     const cache = require('../').cache;
+                     cache.setCache(token,user.toJSON()[0]);
+                     out.token = token;
+                 }
+                 else{
+                     out.msg = 'email  o password non validi'
+                 }
+     }
          cb(out);
 
      })
--- tests/login.js
+++ tests/login.js
@@ -1,14 +1,10 @@
 const tap = require('tap')
 tap.test('login',function(t){
     const cb = function(results){
-        t.ok(results.logged,'log-in successfull')
-        if(results.logged){
+        t.ok(results.user_verified,'log-in successfull')
+        if(results.user_logged){
             t.ok(results.token,'token generato')
             t.ok(require('../core/server/utilities').cache.retrieve(results.token),'token valido')
-
-        }
-        else{
-            t.notOk(results.token)
 
         }
         t.end()
