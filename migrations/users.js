045fb20657d474ca543f5b99612ab968e0cadb21
arpho <damicogiuseppe77@gmail.com>
2017-04-24 18:48:01 +0200

    non è possibile eliminare la migrations per aggiunmgere il campo salt alla tabella users

--- migrations/users.js
+++ migrations/users.js
@@ -1,24 +1,24 @@
 'use strict';
 
-exports.up = function(knex,Promise) {
-  return Promise.all([
-    knex.schema.createTableIfNotExists('users',function(table) {
-        table.uuid('_id').notNullable().primary();
-        table.string('email').notNullable().unique();
-        table.string('password').notNullable().defaultTo('');
-        table.string('fullname').notNullable().defaultTo('');
-        table.string('slug').notNullable().defaultTo('').unique();
-        table.string('url').notNullable().defaultTo('');
-        table.string('location').notNullable().defaultTo('');
-        table.text('description').notNullable().defaultTo('');
-        table.string('role').notNullable().defaultTo('author');
-        table.timestamps();
-    })
-  ]);
+exports.up = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.createTableIfNotExists('users', function (table) {
+            table.uuid('_id').notNullable().primary();
+            table.string('email').notNullable().unique();
+            table.string('password').notNullable().defaultTo('');
+            table.string('fullname').notNullable().defaultTo('');
+            table.string('slug').notNullable().defaultTo('').unique();
+            table.string('url').notNullable().defaultTo('');
+            table.string('location').notNullable().defaultTo('');
+            table.text('description').notNullable().defaultTo('');
+            table.string('role').notNullable().defaultTo('author');
+            table.timestamps();
+        })
+    ]);
 };
 
-exports.down = function(knex,Promise) {
-  return Promise.all([
-    knex.schema.dropTable('users')
-  ]);
+exports.down = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('users')
+    ]);
 };
