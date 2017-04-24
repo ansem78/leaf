2e21edb6b1d3eba91a59ba34eb9324764b2ed123
arpho <damicogiuseppe77@gmail.com>
2017-04-24 18:51:14 +0200

    eliminato controllo per esistenza utente owner nel server

--- index.js
+++ index.js
@@ -14,40 +14,41 @@ app.use(bodyParser.json());
 
 app.use(favicon(__dirname + '/favicon.ico'));
 
-app.use('/admin',express.static(__dirname + '/core/server/admin'));
+app.use('/admin', express.static(__dirname + '/core/server/admin'));
 
-app.engine('hbs',hbs.express4({
-  defaultLayout : config.get('paths:contentPath') + '/themes/default/default',
-  layoutsDir : config.get('paths:contentPath') + '/themes/default',
-  partialsDir : config.get('paths:contentPath') + '/themes/default/partials'
+app.engine('hbs', hbs.express4({
+    defaultLayout: config.get('paths:contentPath') + '/themes/default/default',
+    layoutsDir: config.get('paths:contentPath') + '/themes/default',
+    partialsDir: config.get('paths:contentPath') + '/themes/default/partials'
 }));
 
-app.set('view engine','hbs');
-app.set('views',config.get('paths:contentPath') + '/themes/default');
+app.set('view engine', 'hbs');
+app.set('views', config.get('paths:contentPath') + '/themes/default');
 
 const api = require(__dirname + '/core/server/routes/api');
-app.use('/api',api);
+app.use('/api', api);
 
 const admin = require(__dirname + '/core/server/routes/admin');
-app.use('/admin',admin);
+app.use('/admin', admin);
 
 const site = require(__dirname + '/core/server/routes/site');
-app.use('/',site);
+app.use('/', site);
 
 //const bookshelf = require('./core/server/db');
 const Users = require('./core/server/models/users');
-const launchServer = function(){
+const launchServer = function () {
     const port = process.env.PORT || config.get('server:port');
 
-    const server = app.listen(port,config.get('server:host'),function() {
-      console.log(chalk.green('App listening at %s on port %d.'),server.address().address,server.address().port);
+    const server = app.listen(port, config.get('server:host'), function () {
+        console.log(chalk.green('App listening at %s on port %d.'), server.address().address, server.address().port);
     });
 }
-new Users().query({where:{role:'owner'}}).fetchAll().then((users)=>{//console.log(JSON.stringify(users));
+launchServer();
+/*new Users().query({where:{role:'owner'}}).fetchAll().then((users)=>{//console.log(JSON.stringify(users));
   //console.log(users.toJSON());
   if (users.toJSON().length == 0){
         console.log("occorre creare l'utente owner")
         require('./core/server/utilities/user').createOwner(launchServer)
     }
   else launchServer();
-})
+})*/
