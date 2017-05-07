d778614277bfc34b9f5a61785584de4fa640231d
Alexandro Morato <alexandro1@email.it>
2017-05-07 12:27:50 +0200

    Aggiunta nuova pagina + controller

---
+++ core/server/admin/app/components/labs/labs.js
@@ -0,0 +1,11 @@
+define(['angular'],function(angular) {
+
+  var app = angular.module('Leaf');
+
+  app.register.controller('labsController',['$rootScope','$scope',function($rootScope,$scope) {
+
+
+
+  }]);
+
+});
