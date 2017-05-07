dfb346df4694910b55bff3a01ba2536650140608
Alexandro Morato <alexandro1@email.it>
2017-05-07 12:25:30 +0200

    Aggiunte validazioni Angular

--- core/server/admin/app/components/setup/setup.js
+++ core/server/admin/app/components/setup/setup.js
@@ -2,7 +2,14 @@ define(['angular'],function(angular) {
 
   var app = angular.module('Leaf');
 
-  app.register.controller('setupController',['$rootScope','$scope',function($rootScope,$scope) {
+  app.register.controller('setupController',['$rootScope','$scope','$http',function($rootScope,$scope,$http) {
+
+
+
+    // Installa il sito.
+    $scope.setup = function() {
+
+    };
 
 
 
