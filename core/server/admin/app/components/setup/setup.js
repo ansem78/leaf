1ee2871dfa18d16c18f19267305be9c24bdc18e6
Alexandro Morato <alexandro1@email.it>
2017-05-09 00:45:03 +0200

    Modificati controller e HTML

--- core/server/admin/app/components/setup/setup.js
+++ core/server/admin/app/components/setup/setup.js
@@ -2,13 +2,15 @@ define(['angular'],function(angular) {
 
   var app = angular.module('Leaf');
 
-  app.register.controller('setupController',['$rootScope','$scope','$http',function($rootScope,$scope,$http) {
+  app.register.controller('setupController',['$rootScope','$scope','$http','$state',function($rootScope,$scope,$http,$state) {
 
 
 
     // Installa il sito.
     $scope.setup = function() {
+      if ($scope.form.$valid)  {
 
+      }
     };
 
 
