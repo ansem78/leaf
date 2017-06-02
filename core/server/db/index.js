522e04ae0c9c87f02f0f3c49af2e74cf83ab56da
Alexandro Morato <alexandro1@email.it>
2017-06-03 00:41:17 +0200

    Delete index.js

--- core/server/db/index.js
+++
@@ -1,6 +0,0 @@
-const config = require('../config');
-var db = config.get('database');
-db.connection.filename = __dirname + db.connection.filename;
-const knex = require('knex')(db); // knex ha bisogno del path completo
-const bookshelf = require('bookshelf')(knex);
-module.exports = bookshelf;
