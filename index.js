6579ff5fc00c9bc4c0ac1e916c0bbc9a2ff65d16
Alexandro Morato <alexandro1@email.it>
2017-06-25 12:53:33 +0200

    Implementato provvisoriamente i18n

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
-const config = require('./core/server/config');
-const bodyParser = require('body-parser');
-const express = require('express');
-const hbs = require('express-hbs');
-const favicon = require('serve-favicon');
-const netjet = require('netjet');
-const chalk = require('chalk');
 
-const app = express();
+var express = require('express'),
+bodyParser = require('body-parser'),
+i18n = require('i18n'),
+hbs = require('express-hbs'),
+favicon = require('serve-favicon'),
+netjet = require('netjet'),
+chalk = require('chalk'),
+    
+config = require('./core/server/config'),
+
+app;
+
+i18n.configure({
+  directory : __dirname + '/' + config.get('paths:corePath') + '/server/locales',
+  autoReload : true,
+  api : {
+    '__' : 't',
+    '__n' : 'tn'
+  }
+});
+
+app = express();
 
 app.use(netjet());
 
 app.use(bodyParser.json());
 
+app.use(i18n.init);
+
 app.use(favicon(__dirname + '/favicon.ico'));
 
-app.use('/admin', express.static(__dirname + '/core/server/admin'));
+app.use('/admin', express.static(__dirname + '/' + config.get('paths:corePath') + '/server/admin'));
 
-app.engine('hbs', hbs.express4({
-    defaultLayout: config.get('paths:contentPath') + '/themes/default/default',
-    layoutsDir: config.get('paths:contentPath') + '/themes/default',
-    partialsDir: config.get('paths:contentPath') + '/themes/default/partials'
+app.engine('hbs',hbs.express4({
+    defaultLayout : config.get('paths:contentPath') + '/themes/default/default',
+    layoutsDir : config.get('paths:contentPath') + '/themes/default',
+    partialsDir : config.get('paths:contentPath') + '/themes/default/partials'
 }));
 
-app.set('view engine', 'hbs');
-app.set('views', config.get('paths:contentPath') + '/themes/default');
+app.set('view engine','hbs');
+app.set('views',config.get('paths:contentPath') + '/themes/default');
 
-const api = require(__dirname + '/core/server/routes/api');
-app.use('/api', api);
+var api = require(__dirname + '/' + config.get('paths:corePath') + '/server/routes/api');
+app.use('/api',api);
 
-const admin = require(__dirname + '/core/server/routes/admin');
-app.use('/admin', admin);
+var admin = require(__dirname + '/' + config.get('paths:corePath') + '/server/routes/admin');
+app.use('/admin',admin);
 
-const site = require(__dirname + '/core/server/routes/site');
-app.use('/', site);
+var site = require(__dirname + '/' + config.get('paths:corePath') + '/server/routes/site');
+app.use('/',site);
 
-//const bookshelf = require('./core/server/db');
+var launchServer = function() {
+    var port = process.env.PORT || config.get('server:port');
 
-const launchServer = function () {
-    const port = process.env.PORT || config.get('server:port');
-
-    const server = app.listen(port, config.get('server:host'), function () {
-        console.log(chalk.green('App listening at %s on port %d.'), server.address().address, server.address().port);
+    var server = app.listen(port,config.get('server:host'),function() {
+        console.log(chalk.green('App listening at %s on port %d.'),server.address().address,server.address().port);
     });
-}
+};
+
 launchServer();
